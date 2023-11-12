6954ea9e9bc495eebb2d966c7e81dee694855140
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-11-12 20:05:48 +0900

    Title: 격자 밟기, Time: 113ms, Memory: 9MB, Status: Passed - CodeTree

--- 231112/격자 밟기/stepping-on-grid.java
+++ 231112/격자 밟기/stepping-on-grid.java
@@ -1,5 +1,65 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-    }
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int K, cnt, ans, map[][];
+	static int[] dx = { 0, 0, -1, 1 };
+	static int[] dy = { -1, 1, 0, 0 };
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		map = new int[5][5];
+		K = Integer.parseInt(br.readLine());
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			map[Integer.parseInt(st.nextToken()) - 1][Integer.parseInt(st.nextToken()) - 1] = 1;
+		}
+		if (K % 2 == 1)
+			sb.append(0);
+		else {
+			ans =0;
+			cnt = (25 - K) / 2 + 1;
+			map[0][0] = 1;
+			dfs(0, 0, 1);
+			sb.append(ans);
+		}
+		System.out.println(sb.toString());
+
+	}
+
+	private static void dfs(int x, int y, int dep) {
+		if (dep == cnt) {
+			dfs2(x,y,1);
+			return;
+		}
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || ny < 0 || nx > 4 || ny > 4 || map[nx][ny] == 1 || (nx==4 && ny==4)) 
+				continue;
+			map[nx][ny] = 1;
+			dfs(nx, ny, dep + 1);
+			map[nx][ny] = 0;
+		}
+	}
+
+	private static void dfs2(int x, int y, int dep) {
+		if(dep==cnt) {
+			if(x==4 && y==4)
+				ans++;
+			return;
+		}
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || ny < 0 || nx > 4 || ny > 4 || map[nx][ny] == 1) 
+				continue;
+			map[nx][ny] = 1;
+			dfs2(nx, ny, dep + 1);
+			map[nx][ny] = 0;
+		}
+	}
+
 }
