34434a2c93a7b2932debb5f7a0f201e8817e5da7
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-12-10 22:26:05 +0900

    Title: 격자 늘리기, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231210/격자 늘리기/stretch-grid.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N,M,K;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				char tmp = str.charAt(j);
+				for (int k = 0; k < K; k++) {
+					sb.append(tmp);
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
